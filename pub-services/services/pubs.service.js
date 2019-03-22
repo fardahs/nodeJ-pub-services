9ed4b8075ed5a172bca7e1567b4b55bb74785f27
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-03-22 16:30:50 +0100

    modèle avec des classes

--- pub-services/services/pubs.service.js
+++ pub-services/services/pubs.service.js
@@ -1,9 +1,47 @@
 var pubs = require('../mocks/pubs.json');
-const _= require('lodash');
+
+class Pubs{
+    constructor(name, owner, openHours, beers, openDays){
+        this.name = name
+        this.owner = owner
+        this.openHours = openHours
+        this.openDays = openDays
+        this.beers = beers
+
+    }
+}
+
+class Owner{
+    constructor(firstName, lastName, mail){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.mail = mail
+    }
+}
+
+class OpenHours{
+    constructor(start, end){
+        this.start = start
+        this.end = end
+    }
+}
+
+class Beers{
+    constructor(type, name){
+        this.type = type
+        this.name = name
+    }
+}
 
 function listPub(){
-   var listes =  _.map(pubs, 'name');
-    return listes;
+    console.log(pubs);
+    return pubs.map(items => new Pubs(
+    items.name,
+    new Owner(items.owner.firstName, items.owner.lastName, items.owner.mail),
+    new OpenHours(items.openHours.start, items.openHours.end),
+    items.beers.map(item => new Beers(item.type, item.name)),
+    items.openDays)
+    );
 }
 
 module.exports = {
