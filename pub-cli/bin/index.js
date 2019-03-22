4dc48fbc1c6ed416137cdf6fac6369693addad19
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-03-22 09:39:17 +0100

    delete

--- pub-cli/bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var program = require('commander');
-program
-    .version('1.0.0')
-    .option('-l, --list', 'Get the pubs list')
-    .parse(process.argv);
-if (program.list) {
-    require('../src/main').getListPub();
-}
